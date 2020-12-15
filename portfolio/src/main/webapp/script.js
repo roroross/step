b22eecd2178a42981c1dd842b2872ea45784fe89
Rosanna Liu <mushroompotato123@gmail.com>
2020-12-15 02:50:39 +0000

    commiting recent changes

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,41 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+function random_fun_fact() {
+    const facts = 
+        ['I watched the Star Wars series for the first time this year :")', 
+        'I had a mushroom bob hair cut for 12 years - all the way from primary to the end of high school.', 
+        'During quarantine I picked up vegetable gardening! I grew 10 tomatoes :D ',
+        'I used to have 4 chickens, and one night there was a chicken massacre from a fox(?!). Only one survived. :(',
+        'One time when I was walking around the block, a possum climbed up my leg onto my back! I was so surprised I strained my neck LOL.',
+        'I was one of the first female cadets in a tradionally male only Army Cadet Unit. I then became the first female officer in the Company Command.' ];
+    
+    //Pick a random greeting. random returns random float from 0 - 1. times by length of the list so it gives us it in the range
+    //0.x * length will always be between 0 - length
+    const fun_fact = facts[Math.floor(Math.random() * facts.length)];
+
+    // Add it to the page
+    const fact_container = document.getElementById('fun_fact_container');
+    fact_container.innerText = fun_fact;
+}
+
+
+/**
+ * Generates a URL for a random image in the images directory and adds an img
+ * element with that URL to the page.
+ */
+function random_image() {
+  // The images directory contains 12 images, so generate a random index between
+  // 1 and 12.
+  const imageIndex = Math.floor(Math.random() * 12) + 1;
+  const imgUrl = 'images/Rosanna' + imageIndex + '.jpg';
+
+  const imgElement = document.createElement('img');
+  imgElement.src = imgUrl;
+
+  const imageContainer = document.getElementById('random_image_container');
+  // Remove the previous image.
+  imageContainer.innerHTML = '';
+  imageContainer.appendChild(imgElement);
+}
